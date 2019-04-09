57893fa90fee77910610043bd4ff583625c10d5c
Geoff Kendal <geoff@geftop.fritz.box>
2019-04-09 22:06:49 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# manhuntgps-serverless-websockets
+
+Using the serverless framework, we deploy the following:
+ - Lambda functions (Python 3.7)
+ - API Gateway (Websockets)
+ - DynamoDB
+
+*onConnect* When a client connects for the first time, we send details of all other currently connected players to them. We also add a row for the new client into the database.
+
+*updateLocation* When a client sends a location update, we loop over all rows in the database, and send this update out to all other players. We also handle 401 errors (dead connection), removing these from the database.
+
+*onDisconnect* When a client disconnects, we remove them from the database.
+
+
+```
+sls plugin install -n serverless-python-requirements
+sls deploy
+```
---
+++ handler.py
@@ -0,0 +1,55 @@
+import boto3
+import json
+import os
+
+TABLE_NAME = os.environ['TABLE_NAME']
+ddb = boto3.client('dynamodb')
+
+
+def onConnect(event, context):
+    # TODO: send full list of connected clients here
+    connectionId = event['requestContext']['connectionId']
+    ddb.put_item(TableName=TABLE_NAME, Item={'connectionId':{'S':connectionId}})
+    print('[INFO]: New client connected. Adding to database ' + connectionId)
+    return {
+        "statusCode": 200,
+        "body": 'ok'
+    }
+
+
+def onDisconnect(event, context):
+    connectionId = event['requestContext']['connectionId']
+    ddb.delete_item(TableName=TABLE_NAME, Key={'connectionId':{'S':connectionId}})
+    print('[INFO]: Client disconnected. Removing from database ' + connectionId)
+    return {
+        "statusCode": 200,
+        "body": 'ok'
+    }
+
+
+def updateLocation(event, context):
+    endpoint = 'https://' + event['requestContext']['domainName'] + '/' + event['requestContext']['stage']
+    apigw = boto3.client('apigatewaymanagementapi', endpoint_url=endpoint)
+    
+    message = json.loads(event['body'])
+    body = {
+        "message": message['data']
+    }
+
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(TABLE_NAME)
+    response = table.scan()
+
+    for client in response['Items']:
+        print('[INFO]: Relaying message to client ' + client['connectionId'])
+        response = apigw.post_to_connection(Data=json.dumps(body), ConnectionId=client['connectionId'])
+        if (response['ResponseMetadata']['HTTPStatusCode'] == 401):
+            print('[INFO]: Found stale client. Removing from database ' + client['connectionId'])
+            ddb.delete_item(TableName=TABLE_NAME, Key={'connectionId':{'S':client['connectionId']}})
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+
